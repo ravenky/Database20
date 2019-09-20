d15d389b44f7e27e84b5cb14cc9b4c9545575b4f
Ravi Yendamuri <ravi.k.yendamuri@hp.com>
2019-09-20 11:12:09 +0530

    first commit

---
+++ Database20/Credential1.sql
@@ -0,0 +1,12 @@
+﻿CREATE MASTER KEY ENCRYPTION BY PASSWORD = '23987hxJ#KL95234nl0zBe'; 
+GO
+CREATE DATABASE SCOPED CREDENTIAL adwCredential WITH IDENTITY = 'LoaderRC20', SECRET ='PaSSw0rd';
+go
+CREATE EXTERNAL DATA SOURCE [crmalesADWDS]
+    WITH (
+    TYPE = RDBMS,
+    LOCATION = N'ebdev-sls-adwsvr-crm.database.windows.net',
+    DATABASE_NAME = N'ebdev-sls-adw-crm',
+    CREDENTIAL = [adwCredential]
+    );
+GO
---
+++ Database20/Schema1.sql
@@ -0,0 +1,3 @@
+﻿CREATE SCHEMA [ONS]
+    AUTHORIZATION [dbo];
+
